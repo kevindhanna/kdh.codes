5815cd6e2dac0e6a98af009fef30ef8896797d4c
Kevin Hanna <kevindhanna@fastmail.com>
2024-07-26 10:33:04 -0400

    kevfrastructure: let the lambda create cloudfront invalidations

--- kevfrastructure/modules/api/iam.ts
+++ kevfrastructure/modules/api/iam.ts
@@ -1,11 +1,36 @@
 import * as aws from "@pulumi/aws";
+import * as pulumi from "@pulumi/pulumi";
 import { compileDeployWebkevLambdaRole } from "../webkev/iam";
+import { awsAccountId } from "../core";
+import { webkevCFDistributionId } from "../webkev";
 
 const lambdaRoleAttachment = new aws.iam.RolePolicyAttachment(
     "lambda-role-attachment",
     {
         role: compileDeployWebkevLambdaRole,
         policyArn: aws.iam.ManagedPolicy.AWSLambdaBasicExecutionRole,
+    },
+);
+
+const createCloudFrontInvalidation =
+    pulumi.interpolate`arn:aws:cloudfront::${awsAccountId}:distribution/${webkevCFDistributionId}`.apply(
+        (resourceArn) => {
+            return aws.iam.getPolicyDocument({
+                statements: [
+                    {
+                        effect: "Allow",
+                        actions: ["cloudfront:CreateInvalidation"],
+                        resources: [resourceArn],
+                    },
+                ],
+            });
+        },
+    );
+const lambdaCloudFrontRolePolicy = new aws.iam.RolePolicy(
+    "lambda-cloudfront-role-policy",
+    {
+        role: compileDeployWebkevLambdaRole,
+        policy: createCloudFrontInvalidation.json,
     },
 );
 
